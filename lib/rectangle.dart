97b7e7ae403b5ddaeebf23d5610f2b99256fa2db
Eason Lau <easonlaulearningios@gmail.com>
2022-10-22 13:55:35 +0800

    calcualte rectangle area with bug

--- lib/rectangle.dart
+++ lib/rectangle.dart
@@ -2,7 +2,7 @@ part of area;
 
 
 String calculateAreaRect(double width, double height){
-  double result = width * height;
+  double result = width * height + 1;
   final formatter = NumberFormat('#.####');
   return formatter.format(result);
 }
