8ba149eef0094ad8eda3da2c2f04dbce033c3087
Eason Lau <easonlaulearningios@gmail.com>
2022-10-22 13:45:50 +0800

    first commit of package area

---
+++ lib/area.dart
@@ -0,0 +1,24 @@
+library area;
+import 'package:intl/intl.dart';
+part 'rectangle.dart';
+part 'triangle.dart';
+
+// /// A Calculator.
+// class Calculator {
+//   /// Returns [value] plus 1.
+//   int addOne(int value) => value + 1;
+// }
+
+//MOVE TO reactangle.dart
+// String calculateAreaRect(double width, double height){
+//   double result = width * height;
+//   final formatter = NumberFormat('#.####');
+//   return formatter.format(result);
+// }
+
+//MOVE TO triangle.dart
+// String calculateAreaTriangle(double width, double height){
+//   double result = width * height/2;
+//   final formatter = NumberFormat('#.####');
+//   return formatter.format(result);
+// }
